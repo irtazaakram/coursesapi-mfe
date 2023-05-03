9309902b157253d53bdc9e95b8e0d37a97f17e44
irtazaakram <irtaza.akram@arbisoft.com>
2023-05-03 11:02:22 +0500

    Remove stale code

--- src/courses_api/CoursesApi.jsx
+++ src/courses_api/CoursesApi.jsx
@@ -1,7 +1,6 @@
-import { Container } from '@edx/paragon';
-import fetchCoursesApi from './data/fetchCoursesApi';
+import { Container } from "@edx/paragon";
+import fetchCoursesApi from "./data/fetchCoursesApi";
 
-// react component that renders a list of courses using course data from fetch courses api used in src/courses_api/data/fetchCoursesApi.js. uses edx ui toolkit for styling
 const CoursesApi = () => {
   const courses = fetchCoursesApi();
   return (
@@ -17,49 +16,4 @@ const CoursesApi = () => {
   );
 };
 
-// // react component that renders a list of courses using fetch courses api and course card component. uses edx ui toolkit for styling
-// const CoursesApi = () => {
-//   const [courses, setCourses] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     fetchCourses();
-//   }, []);
-
-//   const fetchCourses = async () => {
-//     try {
-//       const response = await fetch('https://courses.edx.org/api/courses/v1/courses/');
-//       if (!response.ok) {
-//         throw Error(response.statusText);
-//       }
-//       const data = await response.json();
-//       setCourses(data.results);
-//       setIsLoading(false);
-//     } catch (err) {
-//       setError(err);
-//       setIsLoading(false);
-//     }
-//   };
-
-//   if (isLoading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>{error.message}</div>;
-//   }
-
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         {courses.map((course) => (
-//           <div className="col-md-4" key={course.id}>
-//             <CourseCard course={course} />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
 export default CoursesApi;
